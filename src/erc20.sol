a504b8de9ab84bbd2c0f419b734f5495154b2e43
KWAME <antwi1496@hotmail.com>
2024-02-07 15:53:17 -0600

    Writing, Deploying and Testing Solidity Contracts

--- src/erc20.sol
+++ src/erc20.sol
@@ -1,21 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+/**
+ * @title An ERC20 Token contract
+ * @author Kwame 4B
+ * @notice Testing out basic functionality
+ */
 contract FourbToken {
     uint256 public totalSupply;
+    // returns standard decimals for tokens
     uint8 public decimals;
     string public name;
     string public symbol;
+    // to track balance of addresses
     mapping(address => uint256) public balanceOf;
+    // tracking allowances
     mapping(address => mapping(address => uint256 _value)) public Allowance;
 
+    // emits the specified details when there's a successful transfer
     event Transfer(address indexed from, address indexed to, uint256 amount);
+    // emits when there's an approval
     event Approve(
         address indexed owner,
         address indexed spender,
         uint256 amount
     );
 
+    /**
+     * Initializing necessary variables
+     */
     constructor(
         string memory _name,
         string memory _symbol,
@@ -29,6 +42,11 @@ contract FourbToken {
         balanceOf[msg.sender] = totalSupply;
     }
 
+    /**
+     * A function that transfers tokens to a valid receiver
+     * @param receiver address of receiver
+     * @param _amount amount sent to receiver
+     */
     function TransferToken(
         address receiver,
         uint256 _amount
@@ -41,6 +59,11 @@ contract FourbToken {
         return true;
     }
 
+    /**
+     * A function to allocate funds to users
+     * @param amount Amount approved to spend
+     * @param Spender Address of account spending approved funds
+     */
     function ApproveSpender(
         uint256 amount,
         address Spender
@@ -50,6 +73,12 @@ contract FourbToken {
         return true;
     }
 
+    /**
+     * A function to transfer tokens from one address to another
+     * @param from Address of sender
+     * @param to address of receiver
+     * @param amount amount to be sent
+     */
     function transferfrom(
         address from,
         address to,
@@ -62,6 +91,10 @@ contract FourbToken {
         return true;
     }
 
+    /**
+     * A function that mints tokens
+     * @param amount amount to be minted
+     */
     function mint(uint256 amount) external returns (bool) {
         balanceOf[msg.sender] += amount;
         totalSupply += amount;
@@ -69,6 +102,10 @@ contract FourbToken {
         return true;
     }
 
+    /**
+     * A function that burns existing tokens
+     * @param amount amount to be burnt from pool
+     */
     function burn(uint256 amount) external returns (bool) {
         balanceOf[msg.sender] -= amount;
         totalSupply -= amount;
