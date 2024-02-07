dff1ea09de1277278718d0a0b9450f390f933267
KWAME <antwi1496@hotmail.com>
2024-02-06 18:35:52 -0600

    Writing, Deploying and Testing Solidity Contracts

---
+++ src/erc20.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract FourbToken {
+    uint256 public totalSupply;
+    uint8 public decimals;
+    string public name;
+    string public symbol;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256 _value)) public Allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approve(
+        address indexed owner,
+        address indexed spender,
+        uint256 amount
+    );
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint256 _initialAmount
+    ) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _initialAmount * (10 ** (decimals));
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function TransferToken(
+        address receiver,
+        uint256 _amount
+    ) external returns (bool) {
+        require(_amount < balanceOf[msg.sender], "Insufficient balance");
+        require(receiver != address(0), "Invalid receiver");
+        balanceOf[msg.sender] -= _amount;
+        balanceOf[receiver] += _amount;
+        emit Transfer(msg.sender, receiver, _amount);
+        return true;
+    }
+
+    function ApproveSpender(
+        uint256 amount,
+        address Spender
+    ) external returns (bool) {
+        Allowance[msg.sender][Spender] = amount;
+        emit Approve(msg.sender, Spender, amount);
+        return true;
+    }
+
+    function transferfrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool) {
+        Allowance[from][msg.sender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+        return true;
+    }
+
+    function mint(uint256 amount) external returns (bool) {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+        return true;
+    }
+
+    function burn(uint256 amount) external returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+        return true;
+    }
+}
