47e4e2933a0fd7f0c8e03b96210e54036aae5a08
KWAME <antwi1496@hotmail.com>
2024-02-13 04:15:32 -0600

    Writing, Deploying and Testing Solidity Contracts

---
+++ test/TestAnotherToken.t.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier:MIT
+pragma solidity 0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {AnotherToken} from "../src/AnotherToken.sol";
+
+/**
+ * @title A test script for the erc20 base contract
+ * @author Kwame 4B
+ * @notice Just a hands on experience
+ */
+
+contract AnotherTokenTest is Test {
+
+}
