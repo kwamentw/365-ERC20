a09f7d9b36e6d4618c8e1d33db007d18c75a2055
KWAME <antwi1496@hotmail.com>
2024-03-13 19:16:06 -0500

    Writing, Deploying and Testing Contracts

--- src/Frontrunning.sol
+++ src/Frontrunning.sol
@@ -6,6 +6,21 @@ pragma solidity 0.8.20;
  * @author Solidity by example
  * @notice it will be commented how this will be frontrunnable
  */
+
+/**
+ *This is how the program will be front ran;
+ * Before that you'd have to know that
+ * transactions take some time before they are mined
+ * Transactions not yet mined are put in the transaction pool
+ * Transactions with higher gas prices are mined first cos miners too have to make profit
+ *
+ * Alice deploys Frontrun with 10 ether
+ * Bod finds the target string that will hash to the target has ("Ethereum")
+ * Bob calls solve(Ethereum) with gas price set to 15 wei
+ * Eve is watching the transaction pool for the answer to be submitted
+ * Eve sees Bobs answer and calls solve(Etheruem) with a higher gas price than boa of 150 wei
+ * Eve's transaction was mined before Bobs transaction & Eve won the 10 ether
+ */
 contract Frontrun {
     bytes32 public constant HASH =
         0x564ccaf7594d66b1eaaea24fe01f0585bf52ee70852af4eac0cc4b04711cd0e2;
