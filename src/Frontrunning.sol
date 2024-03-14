2695bb4ad7967e5945f542cb80e806eb69509539
KWAME <antwi1496@hotmail.com>
2024-03-13 19:08:18 -0500

    Writing, Deploying and Testing Contracts

--- src/Frontrunning.sol
+++ src/Frontrunning.sol
@@ -3,9 +3,22 @@ pragma solidity 0.8.20;
 
 /**
  * @title A Frontrunnable contract
- * @author Kwame 4B
+ * @author Solidity by example
  * @notice it will be commented how this will be frontrunnable
  */
 contract Frontrun {
+    bytes32 public constant HASH =
+        0x564ccaf7594d66b1eaaea24fe01f0585bf52ee70852af4eac0cc4b04711cd0e2;
 
+    constructor() payable {}
+
+    function solve(string memory solution) public {
+        require(
+            hash == keccak256(abi.encodePacked(solution)),
+            "Incorrect answer"
+        );
+
+        (bool sent, ) = (msg.sender).call{value: 10 ether}("");
+        require(sent, "failed to send ether");
+    }
 }
